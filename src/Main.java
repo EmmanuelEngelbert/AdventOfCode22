47a89a3da477140ceae99e8bf295f6c93294aad3
Emmanuel <emmanuel.engelbert@outlook.com>
2022-12-06 22:47:07 +0000

    Modified Main.java and StringInputs.java to include solution for Day 4 part 1

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ import solutions.Day1;
 import solutions.Day2;
 
 import static solutions.Day3.*;
+import static solutions.Day4.completeOverlap;
 import static solutions.StringInputs.*;
 
 public class Main {
     public static void main(String[] args) {
-       Day1.day1Task(input1);
-       System.out.println(Day2.rpsResult(input2));
-       Day1.top3Cals(input1);
-       System.out.println(Day2.rpsResult2(input2));
-       System.out.println(prioritySum(priorityList(input3)));
-       System.out.println(priorityGroupsOf3(input3));
+       Day1.day1Task(input1); //day 1 - part 1
+       Day1.top3Cals(input1); //day 1 - part 2
+       System.out.println(Day2.rpsResult(input2)); //day 2 - part 1
+       System.out.println(Day2.rpsResult2(input2)); //day 2 - part 2
+       System.out.println(prioritySum(priorityList(input3))); //day 3 -part 1
+       System.out.println(priorityGroupsOf3(input3)); //day 3 - part 2
+       System.out.println(completeOverlap(input4)); //day 4 - part 1
     }
 }
